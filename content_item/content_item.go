6081b243481798bda48d6171bc983f62d8e4ea14
mieubrisse <mieubrisse@gmail.com>
2023-03-26 13:30:59 -0300

    Another checkpoint

--- content_item/content_item.go
+++ content_item/content_item.go
@@ -1,6 +1,62 @@
 package content_item
 
+import (
+	"fmt"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
 type Model struct {
 	name string
 	tags []string
+
+	// Means that a checkbox will be displayed next to the item
+	isSelected bool
+
+	// Means that the item will be displayed
+	isHighlighted bool
+}
+
+func (m Model) Init() tea.Cmd {
+	return nil
+}
+
+func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	return m, nil
+}
+
+func (m Model) View() string {
+	maybeCheckmark := " "
+	if m.isSelected {
+		maybeCheckmark = "✔️"
+	}
+
+	style := lipgloss.NewStyle()
+	if m.isHighlighted {
+		// TODO make color a constant
+		style = style.Bold(true).Foreground(lipgloss.Color("#4e9a06"))
+	}
+
+	value := fmt.Sprintf("%s  %s", maybeCheckmark, m.name)
+	return style.Render(value)
+}
+
+func (m *Model) GetName() string {
+	return m.name
+}
+
+func (m *Model) IsSelected() bool {
+	return m.isSelected
+}
+
+func (m *Model) SetSelected(newValue bool) {
+	m.isSelected = newValue
+}
+
+func (m *Model) IsHighlighted() bool {
+	return m.isHighlighted
+}
+
+func (m *Model) SetHighlighted(newValue bool) {
+	m.isHighlighted = newValue
 }
--- filterable_content_list/filterable_content_list.go
+++ filterable_content_list/filterable_content_list.go
@@ -1,7 +1,6 @@
 package filterable_content_list
 
 import (
-	"fmt"
 	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/mieubrisse/cli-journal-go/content_item"
@@ -43,28 +42,38 @@ func (model Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		switch msg.String() {
 		case "j":
 			newCursorIdx := model.cursorIdx + 1
-			if newCursorIdx < len(model.content) {
-				model.cursorIdx = newCursorIdx
+			if newCursorIdx >= len(model.content) {
+				return model, nil
 			}
+
+			model.content[model.cursorIdx].SetHighlighted(false)
+			model.content[newCursorIdx].SetHighlighted(true)
+			model.cursorIdx = newCursorIdx
+			return model, nil
 		case "k":
-			newCursorIdx := model.cursorIdx - 1
-			if newCursorIdx >= 0 {
-				model.cursorIdx = newCursorIdx
+			newCursorIdx := model.cursorIdx + 1
+			if newCursorIdx < 0 {
+				return model, nil
 			}
+
+			model.content[model.cursorIdx].SetHighlighted(false)
+			model.content[newCursorIdx].SetHighlighted(true)
+			model.cursorIdx = newCursorIdx
+			return model, nil
 		case "x":
-			if model.selectedItemIndexes.Contains(model.cursorIdx) {
-				model.selectedItemIndexes.Remove(model.cursorIdx)
-			} else {
-				model.selectedItemIndexes.Add(model.cursorIdx)
-			}
+			cursorItem := model.content[model.cursorIdx]
+			cursorItem.SetSelected(!cursorItem.IsSelected())
+			return model, nil
 		case "s":
 			// Select all
-			for idx := range model.content {
-				model.selectedItemIndexes.Add(idx)
+			for _, item := range model.content {
+				item.SetSelected(true)
 			}
 		case "d":
 			// Deselect all
-			model.selectedItemIndexes.Clear()
+			for _, item := range model.content {
+				item.SetSelected(false)
+			}
 		case "c":
 			// Clear the filter
 			model.filterInput.SetValue("")
@@ -76,32 +85,26 @@ func (model Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (model Model) View() string {
+	resultBuilder := strings.Builder{}
+
 	escapedFilterText := regexp.QuoteMeta(model.filterInput.Value())
 	nameSearchTerms := strings.Fields(escapedFilterText)
 	// The (?i) makes the search case-insensitive
 	regexStr := "(?i)" + strings.Join(nameSearchTerms, ".*")
 	// Okay to use MustCompile here because we quote the user's input so it should be safe
 	matcher := regexp.MustCompile(regexStr)
-	for idx, item := range model.content {
-		if !matcher.MatchString(item.name) {
+	for _, item := range model.content {
+		if !matcher.MatchString(item.GetName()) {
 			continue
 		}
 
-		maybeCheckmark := " "
-		if _, found := model.selectedItemIndexes[idx]; found {
-			maybeCheckmark = "✔️"
-		}
+		renderedItem := item.View()
 
-		colorizedItemName := termenvOutput.String(item.name)
-		if idx == model.cursorIdx {
-			colorizedItemName = colorizedItemName.Foreground(cursorItemGreen).Bold()
-		}
-
-		row := fmt.Sprintf(" %s  %s", maybeCheckmark, colorizedItemName)
-
-		resultBuilder.WriteString(row + "\n")
+		resultBuilder.WriteString(" " + renderedItem + "\n")
 	}
 	resultBuilder.WriteString("\n")
+
+	return resultBuilder.String()
 }
 
 func (model *Model) Focus() {
